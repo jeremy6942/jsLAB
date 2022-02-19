92a17be809c394fc6b675d4890a4837408d72114
jeremy6942 <99541452+jeremy6942@users.noreply.github.com>
2022-02-19 14:14:21 +0800

    Create Forloop.js

---
+++ Forloop.js
@@ -0,0 +1,35 @@
+let students= ["ali", "abu", "muthu", "ah lee", "ah ming", "shan"]
+
+
+for (let i=0; i<students.length; i++){
+
+
+  students[i]= students[i]+i
+
+
+
+}
+console.log(students)
+
+// student.push("Jeremey")
+
+// for(let index=0; index< student.length; index++){
+
+//     console.log(student[index], "is a boy")
+
+//     console.log("student",index,student[index])
+// }
+
+// student.shift()
+
+// for(let i = 0; i <student.length; i+=2 ){
+
+//     let firstLetter = student[i][0]
+
+//     if(firstLetter== "a")
+//     {
+//     console.log(student[i])
+// }
+// else{
+//     console.log("is not start with a")}
+// }
