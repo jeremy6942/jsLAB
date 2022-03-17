8d0e85df45fb22b587a1e08a0ecf910aaefe4823
jeremy6942 <99541452+jeremy6942@users.noreply.github.com>
2022-03-17 17:26:11 +0800

    Update Assignmment2.js

--- Assignmment2.js
+++ Assignmment2.js
@@ -50,6 +50,9 @@ if (inputGender == "F") {
 
 if(parseInt(inputAge)<=12){
 //The parseInt method parses a value as a string and returns the first integer.
+//parseInt:该函数表示解析一个字符串,并返回一个整数.语法 parseInt(string, radix) 
+//参数 描述 string 必需.要被解析的字符串. radix 可选.表示要解析的数字的基数,
+//该值介于 2 ~ 36 之间.如果省略该参数或其值为 0,则数字将以 10 为基础...
   age =  "you are a kid, "
 } 
 else if((parseInt(inputAge)>12) && (parseInt(inputAge)<=18)){
